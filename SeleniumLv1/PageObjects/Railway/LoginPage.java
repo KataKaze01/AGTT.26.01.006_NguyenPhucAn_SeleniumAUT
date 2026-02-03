a68719e88e4bfa784d935cbb505d4dc586afe25b
KataKaze <fanmu2122@gmail.com>
2026-02-03 16:25:15 +0700

    push file Selenium Practice

---
+++ SeleniumLv1/PageObjects/Railway/LoginPage.java
@@ -0,0 +1,34 @@
+package Railway;
+
+import Constant.Constant;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage extends GeneralPage{
+    private final By _txtUsername = By.xpath("//input[@id='username']");
+    private final By _txtPassword = By.xpath("//input[@id='password']");
+    private final By _btnLogin = By.xpath("//input[@value='login']");
+    private final By _lblLoginErrorMsg = By.xpath("//p[@class='message error LoginForm]");
+
+    public WebElement getTxtUsername(){
+        return Constant.WEBDRIVER.findElement(_txtUsername);
+    }
+    public WebElement getTxtPassword(){
+        return Constant.WEBDRIVER.findElement(_txtPassword);
+    }
+    public WebElement getBtnLogin(){
+        return Constant.WEBDRIVER.findElement(_btnLogin);
+    }
+    public WebElement getlBlLoginErrorMsg(){
+        return Constant.WEBDRIVER.findElement(_lblLoginErrorMsg);
+    }
+
+    public HomePage login(String username, String password){
+        getTxtUsername().sendKeys(username);
+        getTxtPassword().sendKeys(password);
+        getBtnLogin().click();
+
+
+        return new HomePage();
+    }
+}
---
+++ SeleniumLv1/TestCases/Railway/LoginTest.java
@@ -0,0 +1,42 @@
+package Railway;
+
+import Constant.Constant;
+import Utilities.Utilities;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("Pre-condition");
+
+        Constant.WEBDRIVER = new ChromeDriver();
+        Constant.WEBDRIVER.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("Post-condition");
+
+        Constant.WEBDRIVER.quit();
+    }
+
+    @Test
+    public void TC01(){
+        System.out.println("TC01 - User can log into Railway with valid username and password");
+        HomePage homePage = new HomePage();
+        homePage.open();
+
+        LoginPage loginPage = homePage.gotoLoginPage();
+
+        homePage = loginPage.login(Constant.USERNAME, Constant.PASSWORD);
+        String actualMsg =  homePage.getWelcomeMessage();
+        String expectedMsg = "Welcome " + Constant.USERNAME;
+
+        Assert.assertEquals(actualMsg, expectedMsg, "Welcome message is not displayed as expected");
+    }
+}
