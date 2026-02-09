769d079492b0fbc2a5fa8d374994b7a09a0e82b7
KataKaze <fanmu2122@gmail.com>
2026-02-09 21:45:13 +0700

    update steps TC12

--- SeleniumLv1/TestCases/Railway/BookTicketTest.java
+++ SeleniumLv1/TestCases/Railway/BookTicketTest.java
@@ -12,22 +12,31 @@ public class BookTicketTest extends BaseTest {
         System.out.println("User can book 1 ticket at a time");
         System.out.println("Pre-condition: an actived account is existing");
 
+        System.out.println("1. Navigate to QA Railway Website");
         HomePage homePage = new HomePage();
         homePage.open();
         UserAccount userAccount = new UserAccount(Constant.USERNAME, Constant.PASSWORD, "", "");
 
+        System.out.println("2. Login with a valid account ");
         LoginPage loginPage = homePage.gotoLoginPage();
         homePage = loginPage.login(userAccount);
 
+        System.out.println("3. Click on \"Book ticket\" tab");
         BookTicketPage bookTicketPage = homePage.gotoTabBookTicketPage();
 
+        System.out.println("4. Select the next 2 days from \"Depart date\"");
+        System.out.println("5. Select Depart from \"Nha Trang\" and Arrive at \"Huế\"");
+        System.out.println("6. Select \"Soft bed with air conditioner\" for \"Seat type\"");
+        System.out.println("7. Select \"1\" for \"Ticket amount\"");
+        System.out.println("8. Click on \"Book ticket\" button");
         bookTicketPage.bookTicketForm();
 
+        System.out.println("Message \"Ticket booked successfully!\" displays.");
         String actualMsg = bookTicketPage.getLblTicketBookedSuccessMsg().getText();
         String expectedMsg = "Ticket booked successfully!";
         Assert.assertEquals(actualMsg, expectedMsg);
 
-
+        System.out.println("Ticket information display correctly (Depart Date, Depart Station, Arrive Station, Seat Type, Amount)");
         String actualDepartStation = bookTicketPage.getCellDepartStation().getText();
         String actualArriveStation = bookTicketPage.getCellArriveStation().getText();
         String actualSeatType = bookTicketPage.getCellSeatType().getText();
@@ -45,6 +54,5 @@ public class BookTicketTest extends BaseTest {
         Assert.assertEquals(actualSeatType, expectedSeatType);
         Assert.assertEquals(actualDepartDate, expectedDepartDate);
         Assert.assertEquals(actualAmount, expectedAmount);
-
     }
 }
