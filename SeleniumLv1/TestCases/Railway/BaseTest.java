6d236a550a34cb29bb12b07c115a6a6167ac209d
KataKaze <fanmu2122@gmail.com>
2026-02-11 13:37:00 +0700

    update create testng.xml and update parameter in BaseTest

--- SeleniumLv1/TestCases/Railway/BaseTest.java
+++ SeleniumLv1/TestCases/Railway/BaseTest.java
@@ -2,17 +2,29 @@ package Railway;
 
 import Constant.Constant;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
+
+import static com.google.common.base.Ascii.equalsIgnoreCase;
 
 public class BaseTest {
     @Parameters("Browser")
-    @BeforeMethod
-    public void beforeMethod(){
-        System.out.println("Pre-condition");
 
-        Constant.WEBDRIVER = new ChromeDriver();
+    @BeforeMethod
+    public void beforeMethod(@Optional("chrome") String browser){
+        System.out.println("Pre-condition");
+        String runBrowser = System.getProperty("browser", browser);
+
+        if ("chrome".equalsIgnoreCase(runBrowser)){
+            Constant.WEBDRIVER = new ChromeDriver();
+        }else if ("fire fox".equalsIgnoreCase(runBrowser)){
+            Constant.WEBDRIVER = new FirefoxDriver();
+        }else {
+            throw new RuntimeException("Unsupported browser: " + runBrowser);
+        }
         Constant.WEBDRIVER.manage().window().maximize();
     }
 
